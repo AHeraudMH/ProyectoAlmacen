a6d7876f8f82cb49dff75767f5ba5d123084b2b5
Heraud Mendoza Huaraca <u18302063@utp.edu.pe>
2022-12-27 22:40:48 -0500

    Agregando avances Conexion-Utilidades, CrudSimpleInterface-InterfaceDao, CategoriaDAO-Dao

---
+++ src/main/java/Dao/CategoriaDAO.java
@@ -0,0 +1,84 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Dao;
+
+import InterfacesDao.CrudSimpleInterface;
+import Entidades.Categoria;
+import Utilidades.Conexion;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author AHeraudMH
+ */
+public class CategoriaDAO implements CrudSimpleInterface<Categoria> {
+
+    private final Conexion CON;
+    private PreparedStatement ps;
+    private ResultSet rs;
+    private boolean resp;
+
+    public CategoriaDAO() {
+        CON = Conexion.getInstancia();
+    }
+
+    @Override
+    public List<Categoria> listar(String texto) {
+        List<Categoria> registros = new ArrayList();
+        try {
+            ps = CON.conectar().prepareStatement("SELECT * FROM Categoria WHERE nombre LIKE ?");
+            ps.setString(1, "%" + texto + "%");
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                registros.add(new Categoria(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getBoolean(4)));
+            }
+            ps.close();
+            rs.close();
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        } finally {
+            ps = null;
+            ps = null;
+            CON.desconectar();
+        }
+        return registros;
+    }
+
+    @Override
+    public boolean insertar(Categoria obj) {
+        f; //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean actualizar(Categoria obj) {
+        f; //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean desactivar(int id) {
+        f; //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean activar(int id) {
+        f; //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int total() {
+        f; //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean existe(String texto) {
+        f; //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
